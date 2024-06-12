212cf957bc9687e7a09c31a3b27a9c0f69422663
Shaganashree97 <shaganashree.pl@gmail.com>
2024-06-12 07:40:53 +0530

    data frame, x bar chart, r chart

---
+++ 10 Control Charts for Variables(Mean & Range Chart)/x_chart.R
@@ -0,0 +1,15 @@
+S1 = c(46,45,44,43,42)
+S2 = c(41,41,44,42,40)
+S3 = c(40,40,42,40,42)
+S4 = c(42,43,43,42,45)
+S5 = c(43,44,47,47,45)
+
+A = as.data.frame(rbind(S1,S2,S3,S4,S5))
+A
+
+# Install qcc packages
+
+# install.packages("qcc")
+# library(qcc)
+
+xchart = qcc(data=A, type="xbar", sizes=5, title="X bar chart", plot=TRUE)
